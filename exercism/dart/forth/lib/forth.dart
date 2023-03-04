09dd81cc88fdb9a3987896f3c07c366da45dfe8b
Japhet Obsioma <japhetmerto@gmail.com>
2023-03-04 19:41:17 +0800

    WIP

--- exercism/dart/forth/lib/forth.dart
+++ exercism/dart/forth/lib/forth.dart
@@ -1,108 +1,110 @@
 class Forth {
   List<int> stack = [];
 
+  bool isNumber(String value) => int.tryParse(value) != null;
+
+  int add(int x, int y) => x + y;
+
+  int subtract(int x, int y) => x - y;
+
+  int multiply(int x, int y) => x * y;
+
+  int divide(int x, int y) {
+    try {
+      return x ~/ y;
+    } catch (_) {
+      throw Exception('Division by zero');
+    }
+  }
+
+  Map<String, List<String>> userDefinedWords = {};
+
   void evaluate(String value) {
-    final chars = value.split(' ');
-    final numbers = <int>[];
-    final symbols = <String>[];
-    String word = '';
+    List<String> characters = value.split(' ');
 
-    for (final char in chars) {
-      final number = int.tryParse(char);
-
-      if (number != null) {
-        numbers.add(number);
+    if (characters.first == ':' && characters.last == ';') {
+      final definition = characters..removeWhere((e) => e == ':' || e == ';');
+      final value = [...definition]..removeAt(0);
+      if (value.length == 1) {
+        final newValue = userDefinedWords[value.first];
+        userDefinedWords.addAll(
+          {definition.first: newValue ?? value},
+        );
       } else {
-        if (char.length > 1) {
-          word = char;
-        } else {
-          symbols.add(char);
+        userDefinedWords.addAll(
+          {definition.first: value},
+        );
+      }
+      return;
+    }
+
+    if (characters.any((e) => userDefinedWords.containsKey(e))) {
+      for (final character in [...characters]) {
+        if (userDefinedWords.containsKey(character)) {
+          final value = userDefinedWords[character]!;
+          final index = characters.indexWhere((e) => e == character);
+          characters.replaceRange(index, index + 1, value);
         }
       }
     }
 
-    if (symbols.isEmpty) {
-      if (word.isNotEmpty) {
-        if (numbers.isNotEmpty) {
-          stack.addAll(_processWord(word, numbers));
-        } else {
+    for (final character in characters) {
+      if (isNumber(character)) {
+        final number = int.parse(character);
+        stack.add(number);
+      } else if (character.length > 1) {
+        if (stack.isEmpty) throw Exception('Stack empty');
+
+        switch (character) {
+          case 'dup':
+            stack.add(stack.last);
+            break;
+          case 'drop':
+            stack.removeLast();
+            break;
+          case 'swap':
+            if (stack.length < 2) throw Exception('Stack empty');
+            final savedStack = [...stack];
+            stack.removeRange(stack.length - 2, stack.length);
+            stack.addAll([savedStack.last, savedStack[savedStack.length - 2]]);
+            break;
+          case 'over':
+            if (stack.length < 2) throw Exception('Stack empty');
+            if (stack.length < 3) {
+              stack.add(stack.first);
+            } else {
+              stack.add(stack[stack.length - 2]);
+            }
+            break;
+          default:
+            throw UnimplementedError('$character is not implemented');
+        }
+      } else {
+        if (characters.isEmpty || characters.length < 3) {
           throw Exception('Stack empty');
         }
-      } else {
-        stack.addAll(numbers);
-      }
-    } else {
-      if (numbers.length == 1 || numbers.isEmpty) {
-        throw Exception('Stack empty');
-      }
 
-      if (symbols.length == 1) {
-        stack.add(
-          _processArithmetic(symbols.first, numbers.first, numbers.last),
-        );
-      } else {
-        final firstOperation = _processArithmetic(
-          symbols[0],
-          numbers[0],
-          numbers[1],
-        );
-        stack.add(_processArithmetic(symbols[1], firstOperation, numbers.last));
+        final savedStack = [...stack];
+        stack.clear();
+
+        switch (character) {
+          case '+':
+            stack.add(add(savedStack.first, savedStack.last));
+            break;
+          case '-':
+            stack.add(subtract(savedStack.first, savedStack.last));
+            break;
+          case '*':
+            print(stack);
+            stack.add(multiply(savedStack.first, savedStack.last));
+            break;
+          case '/':
+            stack.add(divide(savedStack.first, savedStack.last));
+            break;
+          default:
+            throw UnimplementedError('$character is not implemented');
+        }
       }
-    }
-  }
-
-  List<int> _processWord(String word, List<int> numbers) {
-    print(word);
-    switch (word) {
-      case 'dup':
-        if (numbers.length == 1) {
-          return [numbers.first, numbers.first];
-        } else {
-          return [...numbers, numbers.last];
-        }
-      case 'drop':
-        if (numbers.length == 1) {
-          return [];
-        } else {
-          return [...numbers.take(numbers.length - 1)];
-        }
-      case 'swap':
-        if (numbers.length == 2) {
-          return [numbers.last, numbers.first];
-        } else {
-          return [numbers.first, numbers[2], numbers[1]];
-        }
-      case 'over':
-        if (numbers.length == 2) {
-          return [...numbers, numbers.first];
-        } else {
-          return [...numbers, numbers[numbers.length - 2]];
-        }
-      default:
-        throw UnimplementedError('$word is not implemented');
-    }
-  }
-
-  int _processArithmetic(
-    String symbol,
-    int a,
-    int b,
-  ) {
-    switch (symbol) {
-      case '+':
-        return a + b;
-      case '-':
-        return a - b;
-      case '*':
-        return a * b;
-      case '/':
-        try {
-          return a ~/ b;
-        } on UnsupportedError {
-          throw Exception('Division by zero');
-        }
-      default:
-        throw UnimplementedError('$symbol is not implemented');
     }
   }
 }
